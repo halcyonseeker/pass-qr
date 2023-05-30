20e6a346b62b19b3cddb99cff704ee4ca2eab8e7
ꙮ <ymir@ulthar.xyz>
2023-05-30 19:30:36 +0700

    checked in the code

---
+++ qr.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cmd_qr_help () {
+	cat <<-_EOF
+Usage:
+    $PROGRAM qr [version,--version,-v]
+       Print extension version.
+    $PROGRAM qr [help,--help,-h]
+       Print help for the qr extension.
+    $PROGRAM qr [password names....]
+       Print passwords passed on the command line as QR codes.  It
+       takes the first line of each password file, strips the newline,
+       and prints the UTF-8 QR code to stdout.
+_EOF
+}
+
+cmd_qr_version () {
+	echo "$VERSION"
+}
+
+case "$1" in
+	help|--help|-h)       cmd_qr_help ;;
+	version|--version|-v) cmd_qr_version ;;
+	*) for password in "$@"; do
+		   echo
+		   pass "$password" | tr -d '\n' | qrencode -t UTF8
+		   echo
+		   shift
+	   done ;;
+esac
